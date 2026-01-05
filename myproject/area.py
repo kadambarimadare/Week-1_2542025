09edd7650cd9a67d91f33fb67e5d0102cf16e7d1
kadambarimadare <kadambarimadare@gmail.com>
2026-01-05 14:18:00 +0530

    area.py

---
+++ myproject/area.py
@@ -0,0 +1,11 @@
+def circle(r):
+	print('area of circle  is:',3.14*r*r)
+	
+def rectangle(l,b):
+	print('area of rectangle:',l*b)
+
+def square(side):
+	print('area of square:',side*side)	
+	
+def triangle(b1,h):
+	print('area of triangle:',0.5*b1*h
